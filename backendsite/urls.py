3e9b68af17dec1582f46c44cbfc1cc766c184de5
nahmann <nahmann@ucsd.edu>
2022-11-28 16:37:30 -0800

    quickstart

--- backendsite/urls.py
+++ backendsite/urls.py
@@ -18,5 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('quickstart/', include('quickstart.urls')),
     path('admin/', admin.site.urls),
 ]
+
--- quickstart/urls.py
+++ quickstart/urls.py
@@ -6,8 +6,8 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
-# Use automatic URL routing
-# Can also include login URLs for the browsable API
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
