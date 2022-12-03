784b07c6c5b17853fae0ace71abb91163b79f01d
AlexGuan123 <alexguan626@gmail.com>
2022-12-03 15:45:40 -0800

    Connecting webservice to heroku postgres
    Allowing app to connect to Heroku postgres to store data

--- backendsite/settings.py
+++ backendsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +90,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
