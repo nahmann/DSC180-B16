850ab9ffaccd4594103f188544feb3ea80a74d2e
nahmann <nahmann@ucsd.edu>
2022-12-03 19:28:00 -0800

    Added basenames

--- locationConsensus/urls.py
+++ locationConsensus/urls.py
@@ -3,9 +3,9 @@ from rest_framework import routers
 from locationConsensus import views
 
 router = routers.DefaultRouter()
-router.register(r'interactions', views.InteractionViewSet)
-router.register(r'ephIDs', views.EphIDViewSet)
-router.register(r'locations', views.LocationViewSet)
+router.register(r'interactions', views.InteractionViewSet, basename='interaction')
+router.register(r'ephIDs', views.EphIDViewSet, basename='ephID')
+router.register(r'locations', views.LocationViewSet, basename='location')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- locationConsensus/views.py
+++ locationConsensus/views.py
@@ -1,24 +1,24 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .serializers import *
 from .models import Interaction, EphID, Location
 
 class InteractionViewSet(viewsets.ModelViewSet):
     """
-    API endpoint allows interactions to be viewed or edited.
+    API endpoint that allows interactions to be viewed or edited.
     """
     queryset = Interaction.objects.all().order_by('-time')
     serializer_class = InteractionSerializer
 
 class EphIDViewSet(viewsets.ModelViewSet):
     """
-    API endpoint allows ephIDs to be viewed or edited.
+    API endpoint that allows ephIDs to be viewed or edited.
     """
     queryset = EphID.objects.all().order_by('-ephID')
     serializer_class = EphIDSerializer
 
 class LocationViewSet(viewsets.ModelViewSet):
     """
-    API endpoint allows locations to be viewed or edited.
+    API endpoint that allows locations to be viewed or edited.
     """
     queryset = Location.objects.all().order_by('-location')
     serializer_class = LocationSerializer
