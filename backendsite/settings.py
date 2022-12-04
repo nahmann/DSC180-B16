172521379adc72cf7f7cf891f6f2ed7c5f6692a8
AlexGuan123 <alexguan626@gmail.com>
2022-12-03 16:36:57 -0800

    Change endgine to be postgress

--- backendsite/settings.py
+++ backendsite/settings.py
@@ -88,8 +88,9 @@ WSGI_APPLICATION = 'backendsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
