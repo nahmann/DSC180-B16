c0e758e07edf9e3b4bfb6fe4371edc5b7a2ff83d
nahmann <nahmann@ucsd.edu>
2023-01-27 12:48:55 -0800

    Changed Model Representation

--- locationConsensus/models.py
+++ locationConsensus/models.py
@@ -1,11 +1,24 @@
 from django.db import models
+from django.utils import timezone
+
+class User(models.Model):
+    userID = models.CharField(max_length=20, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.userID
+
+class Location(models.Model):
+    locationID = models.CharField(max_length=20, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.locationID
 
 class Interaction(models.Model):
     interactionID = models.CharField(max_length=20)
-    spotter = models.CharField(max_length=20)
-    spotted = models.CharField(max_length=20)
-    location = models.CharField(max_length=20)
-    time = models.DateTimeField('time occured')
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    spotted_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    time = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.interactionID
