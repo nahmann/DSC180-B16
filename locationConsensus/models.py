dcbb532247c5708954e5f1d9f78dc6871f100dd7
nahmann <nahmann@ucsd.edu>
2023-02-06 14:39:52 -0800

    tried to fix a deploy error

--- locationConsensus/models.py
+++ locationConsensus/models.py
@@ -4,11 +4,11 @@ import json
 
 class User(models.Model):
     userID = models.CharField(max_length=20, primary_key=True, unique=True)
-    verificationStatus = models.CharField(max_length=20, default='default')
+    verificationStatus = models.CharField(max_length=50, default='default')
 
 class Interaction(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='spotter')
-    spotted_users = models.CharField(max_length=20)
+    spotted_users = models.CharField(max_length=50)
     time = models.DateTimeField(default=timezone.now)
 
     def set_spotted_users (self, lst):
